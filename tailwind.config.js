eab45e3be8a241a4cc4fbfb4e4c973e8ab523356
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-08-26 12:19:06 +0100

    Added background images.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'admin-register-img': "url('./src/assets/South-Asian-Novels-To-Add-To-Your-Shelf_Website--1500-x-1024-.jpg')"
+        'admin-register-img': "url('./src/assets/South-Asian-Novels-To-Add-To-Your-Shelf_Website--1500-x-1024-.jpg')",
+        'admin-login-img': "url('./src/assets/2e1c4271-b77c-4d37-87c8-4f6ff27b3459.jfif')"
       }
     },
   },
