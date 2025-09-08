2ed2be3505a129147c1a85767faac1803dc9a61f
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-09-08 09:31:30 +0100

    added color for gold

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,15 @@ export default {
         'custom-striped': 'repeating-linear-gradient(-45deg, transparent, transparent 14px, rgba(0, 0, 0, 0.3) 14px, rgba(0, 0, 0, 0.3) 15px)',
         'admin-register-img': "url('./src/assets/South-Asian-Novels-To-Add-To-Your-Shelf_Website--1500-x-1024-.jpg')",
         'admin-login-img': "url('./src/assets/2e1c4271-b77c-4d37-87c8-4f6ff27b3459.jfif')"
-      }
+      },
+      colors: {
+        gold: '#FFD700', // Define your gold color here
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ['group-hover'],
     },
   },
   plugins: [
