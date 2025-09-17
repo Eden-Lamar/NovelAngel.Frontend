3dd00c3293f3fd1e2fb1923eb193e1c3f491d76c
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-09-17 14:51:22 +0100

    added route for books/:id, books/:bookId/read, /profile

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 import AdminHeader from "../components/admin/AdminHeader";
 import Sidebar from "../components/admin/Sidebar";
 import AdminDashboard from "../pages/AdminDashboard";
+import Profile from "../pages/admin/Profile";
 import Books from "../components/admin/Books";
+import BookReader from '../components/admin/BookReader';
+import BookDetails from "../components/admin/BookDetails";
 import CreateBook from "../features/Admin/CreateBook";  // Import CreateBook page
 import AddChapter from "../features/Admin/AddChapter";  
+
 
 import "../components/admin/admin.css"
 
@@ -27,7 +31,9 @@ const AdminLayout = () => {
 				<Route path="create-book" element={<CreateBook />} />
 				<Route path="add-chapter/:bookId" element={<AddChapter />} />
 				<Route path="books" element={<Books />} />
-				
+				<Route path="books/:id" element={<BookDetails />} />
+				<Route path="books/:bookId/read" element={<BookReader />} />
+				<Route path="profile" element={<Profile />} />
 				</Routes>
 			</div>
 		</div>
