ba97c84381e40f03ca611409f3875353bae41698
Eden-Lamar <emmanuelmomodu3@gmail.com>
2025-10-06 10:12:23 +0100

    Added fontfamily and changed admin-register-img and admin-login-img images

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,14 @@ export default {
     extend: {
       backgroundImage: {
         'custom-striped': 'repeating-linear-gradient(-45deg, transparent, transparent 14px, rgba(0, 0, 0, 0.3) 14px, rgba(0, 0, 0, 0.3) 15px)',
-        'admin-register-img': "url('./src/assets/South-Asian-Novels-To-Add-To-Your-Shelf_Website--1500-x-1024-.jpg')",
-        'admin-login-img': "url('./src/assets/2e1c4271-b77c-4d37-87c8-4f6ff27b3459.jfif')"
+        'admin-register-img': "url('./src/assets/download (1).jfif')",
+        'admin-login-img': "url('./src/assets/download.jfif')"
       },
       colors: {
         gold: '#FFD700', // Define your gold color here
+      },
+      fontFamily: {
+        vibes: ['"Great Vibes"', 'cursive'],
       },
     },
   },
